c9e146fe397f3ba21b67a9d03096cfedcc4ca118
mikehansonn <90873556+mikehansonn@users.noreply.github.com>
2023-06-07 00:05:50 -0400

    cahnges

--- Contain_In_box.py
+++ Contain_In_box.py
@@ -8,6 +8,7 @@ def __init__(self, file_name):
     self.move = TankInit.robot()
     self.gps = SparkfungpsClass41.Gpsclass()
     self.gps_list = []
+    self.equations = [] # make these as tuples but the slope is at 0 and the y intercept is at 1
     append_list = []
 
     with open(file_name, "r") as file:
@@ -25,20 +26,38 @@ def __init__(self, file_name):
                 point = [float(x), float(y)]
                 append_list.append(point)
 
+    make_slope_equations()
     
 
 
-def is_inside_shape(point, shape_coordinates):
-    x, y = point
-    num_vertices = len(shape_coordinates)
-    inside = False
-    j = num_vertices - 1
+def is_inside_shape(self):
+    for i in range(len(self.equations)):
+        for j in range(len(self.equations[i])):
+            coords = self.gps.get_coordinates() # lat in 0, long in 1
 
-    for i in range(num_vertices):
-        if ((shape_coordinates[i][1] > y) != (shape_coordinates[j][1] > y)) and \
-                (x < (shape_coordinates[j][0] - shape_coordinates[i][0]) * (y - shape_coordinates[i][1]) /
-                     (shape_coordinates[j][1] - shape_coordinates[i][1]) + shape_coordinates[i][0]):
-            inside = not inside
-        j = i
 
-    return inside
+
+def make_slope_equations(self):
+    add_list = []
+    for i in range(len(self.gps_list)):
+        for j in range(len(self.gps_list[i])):
+            if j == 0:
+                add_list.append(self.gps_list[i][j])
+            else:
+                if j < len(self.gps_list[i]) - 1:
+                    add_list.append(find_slope(self.gps_list[i][j], self.gps_list[i][j + 1]))
+                else:
+                    add_list.append(find_slope(self.gps_list[i][j], self.gps_list[i][1]))
+
+        self.equations.append(add_list)
+        add_list = []
+
+
+def find_slope(self, p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+
+    m = (y2 - y1) / (x2 - x1)
+    b = y1 - m * x1
+
+    return (m, b)
--- check.py
+++ check.py
@@ -1,5 +1,6 @@
 gps_list = []
 append_list = []
+equations = []
 
 with open("points.txt", "r") as file:
     for line in file:
@@ -16,4 +17,38 @@ with open("points.txt", "r") as file:
             point = [float(x), float(y)]
             append_list.append(point)
 
-print(gps_list)
+def make_slope_equations():
+    add_list = []
+    for i in range(len(gps_list)):
+        for j in range(len(gps_list[i])):
+            if j == 0:
+                add_list.append(gps_list[i][j])
+            else:
+                if j < len(gps_list[i]) - 1:
+                    add_list.append(find_slope(gps_list[i][j], gps_list[i][j + 1]))
+                else:
+                    add_list.append(find_slope(gps_list[i][j], gps_list[i][1]))
+
+        equations.append(add_list)
+        add_list = []
+
+
+def find_slope(p1, p2):
+    print(p1)
+    print(p2)
+    print()
+    x1, y1 = p1
+    x2, y2 = p2
+
+    m = (y2 - y1) / (x2 - x1)
+    b = y1 - m * x1
+
+    return (m, b)
+
+
+
+make_slope_equations()
+print(equations)
+
+
+
